bd5639fa34aa136989de1ad04f55d524d60b5c7d
bubb134 <benfaulkner2005@icloud.com>
2024-10-24 10:37:24 +0100

    Created Player Class
    Created Player Class, holds the funds, income, satisfaction of the player. Also contains the methods to place a building and handle an event.

---
+++ core/src/main/java/uk/ac/york/vfc510/unisim/Player.java
@@ -0,0 +1,43 @@
+package uk.ac.york.vfc510.unisim;
+
+
+public class Player {
+    private int funds;
+    private int income;
+    private int satisfaction;
+
+    public Player() {
+        funds = 100_000; //Initial funds *May need tweak
+        income = 1_000; //Initial income *May need tweak
+        satisfaction = 50; //Initial satisfaction *May need tweak
+    }
+
+    public void placeBuilding(Building building){
+        if(funds>= building.getCost()){
+            funds -= building.getCost();
+            income += building.getIncome();
+            satisfaction += building.getSatisfaction();
+        }
+    }
+
+    public void handleEvent(Events events){
+        funds += events.getEffectOnFunds();
+        income *= events.getIncomeMultiplier();
+    }
+
+    public int getFunds() {
+        return funds;
+    }
+
+    public void setFunds(int funds){
+        this.funds = funds;
+    }
+
+    public int getIncome() {
+        return income;
+    }
+
+    public void setIncome(int income){
+        this.income = income;
+    }
+}

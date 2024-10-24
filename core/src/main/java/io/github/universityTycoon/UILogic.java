54c3c09c23577839fd27935e87da34fb02d9a340
16RoebuW <95746761+16RoebuW@users.noreply.github.com>
2024-10-24 15:59:19 +0100

    UILogic scaffolded

--- core/src/main/java/io/github/universityTycoon/UILogic.java
+++ core/src/main/java/io/github/universityTycoon/UILogic.java
@@ -1,8 +1,12 @@
 package io.github.universityTycoon;
 
+import java.util.HashMap;
+
 public class UILogic {
 
-    public void GenerateEndScreen() {
-
+    public HashMap<String, String> GenerateEndScreen() {
+        // Returns a map of strings to show on the end screen. Add parameters as necessary.
+        // For example: "Satisfaction" : "100", "Students gained" : "-19", etc.
+        return new HashMap<String, String>();
     }
 }

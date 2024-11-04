20c324d68be09c13d15a6e605d050f3bad47868b
16RoebuW <95746761+16RoebuW@users.noreply.github.com>
2024-11-04 14:02:54 +0000

    Fixed timer implementation

--- core/src/main/java/io/github/universityTycoon/Main.java
+++ core/src/main/java/io/github/universityTycoon/Main.java
@@ -1,10 +1,11 @@
 package io.github.universityTycoon;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
-
+    float timeSeconds;
     PlayerInputHandler playerInputHandler = new PlayerInputHandler();
     GameModel gameModel = new GameModel(playerInputHandler);
     UILogic uiLogic = new UILogic();
@@ -15,6 +16,6 @@ public class Main extends Game {
     }
     @Override
     public void render() {
-
+        timeSeconds += Gdx.graphics.getDeltaTime();
     }
 }

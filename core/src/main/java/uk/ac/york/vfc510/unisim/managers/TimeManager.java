20c1f44e4ebe4a8eafea506b6fdb1df3e8bf8083
James Haworth <james@jameshaworth.dev>
2024-11-09 03:10:51 +0000

    Corrected game time limit

--- core/src/main/java/uk/ac/york/vfc510/unisim/managers/TimeManager.java
+++ core/src/main/java/uk/ac/york/vfc510/unisim/managers/TimeManager.java
@@ -8,7 +8,8 @@ public class TimeManager {
     public TimeManager(int timeStep, int currentTime) {
         TimeManager.currentTime = 0;
         this.timeStep = 1;
-        this.timeLimit = 100;
+        this.timeLimit = 300;
+        // 5 minutes = 300 seconds
     }
 
     public void incrementTime() {
